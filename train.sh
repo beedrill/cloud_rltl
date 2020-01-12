a3c984ca6344ce0d8d75828fed18f4cacb95f5b3
XINZE ZHOU <zhouxinze@192.168.0.2>
2020-01-12 15:05:34 +0800

    train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-PYTHON=`which python3`
-for i in {1,2,5,10}
-do
-   PYTHON dqn_training.py --delay_time $i >> delay_time_$i_log.txt
-done
+python3 dqn_training.py --delay_time 1 >> delay_time_1_log.txt
+python3 dqn_training.py --delay_time 2 >> delay_time_2_log.txt
+python3 dqn_training.py --delay_time 3 >> delay_time_3_log.txt
+python3 dqn_training.py --delay_time 5 >> delay_time_5_log.txt
+python3 dqn_training.py --delay_time 10 >> delay_time_10_log.txt
+python3 dqn_training.py --delay_time 20 >> delay_time_20_log.txt
+python3 dqn_training.py --delay_time 30 >> delay_time_30_log.txt
+python3 dqn_training.py --delay_time 50 >> delay_time_50_log.txt
+python3 dqn_training.py --delay_time 100 >> delay_time_100_log.txt
+python3 dqn_training.py --delay_time 150 >> delay_time_150_log.txt
+python3 dqn_training.py --delay_time 200 >> delay_time_200_log.txt
+python3 dqn_training.py --delay_time 300 >> delay_time_300_log.txt
+python3 dqn_training.py --delay_time 400 >> delay_time_400_log.txt
+python3 dqn_training.py --delay_time 500 >> delay_time_500_log.txt
