52992edf0a44bf9aa6d2f28485ebf58eab2eeca3
XINZE ZHOU <zhouxinze@192.168.0.2>
2020-01-13 22:13:03 +0800

    evaluate.sh

--- dqn_evaluation2.py
+++ dqn_evaluation2.py
@@ -25,6 +25,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run Q learning for cloud rltl')
     parser.add_argument('--visual', action='store_true', help='use visualization')
     parser.add_argument('--folder_name', action='store', type=str, help='path of the model parameter')
+    parser.add_argument('--delay_time', action = 'store', default = 0, type = int, help = 'specify the delay time')
     cmd_args = parser.parse_args()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # print(args)
@@ -38,8 +39,8 @@ if __name__ == '__main__':
     screen_height, screen_width = env.observation_space.shape
     n_actions = env.action_space.n
     wait_list = []
-    for i in range(12):
-        filename  = "./params/DRL_%s/net_params_%d.pkl"%(cmd_args.folder_name, i)
+    for i in range(150):
+        filename  = "./params/%s/net_params_%d.pkl"%(cmd_args.folder_name, i)
         target_net = DQN(screen_height, screen_width, n_actions).to(device)
         target_net.load_state_dict(torch.load(filename))
         env.reset()
@@ -48,7 +49,7 @@ if __name__ == '__main__':
     wait_list.sort(key = lambda k:k[1])
     best_network_index = wait_list[0][0]
     # Re-run 5 times, take average
-    filename = "./params/DRL_%s/net_params_%d.pkl" % (cmd_args.folder_name, best_network_index)
+    filename = "./params/%s/net_params_%d.pkl" % (cmd_args.folder_name, best_network_index)
     target_net = DQN(screen_height, screen_width, n_actions).to(device)
     target_net.load_state_dict(torch.load(filename))
     wait_total = 0
@@ -58,7 +59,4 @@ if __name__ == '__main__':
         wait_total += waiting_times[0]
     wait_average = wait_total / 5.0
 
-    with open('./evaluation/DRL_%s_net_params_%d.txt' % (cmd_args.folder_name, best_network_index),'w+') as f:
-        f.write("DRL_%s best network average waiting time %f\n" % (cmd_args.folder_name, wait_average))
-
-    print('DRL_%s best network average waiting time %f\n' % (cmd_args.folder_name, wait_average))
+    print('delay_time %d average waiting time %f\n' % (cmd_args.delay_time, wait_average))
---
+++ evaluate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+python3 dqn_evaluation_2.py --folder_name 'DRL_2' --delay_time 1 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_3' --delay_time 2 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_4' --delay_time 3 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_5' --delay_time 5 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_6' --delay_time 10 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_7' --delay_time 20 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_8' --delay_time 30 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_9' --delay_time 50 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_10' --delay_time 100 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_11' --delay_time 150 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_12' --delay_time 200 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_13' --delay_time 300 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_14' --delay_time 400 >> ./evaluation_result/evaluation_1.txt
+#python3 dqn_evaluation_2.py --folder_name 'DRL_17' --delay_time 500 >> ./evaluation_result/evaluation_1.txt
