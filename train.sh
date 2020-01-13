c31173f419fe41b35c6adab0fcc332a4cd9589a4
XINZE ZHOU <zhouxinze@192.168.0.2>
2020-01-13 23:38:00 +0800

    train.sh

--- train.sh
+++ train.sh
@@ -7,10 +7,10 @@ python3 dqn_training.py --delay_time 5 >> ./training_log/2/delay_time_5_log.txt
 python3 dqn_training.py --delay_time 10 >> ./training_log/2/delay_time_10_log.txt
 python3 dqn_training.py --delay_time 20 >> ./training_log/2/delay_time_20_log.txt
 python3 dqn_training.py --delay_time 30 >> ./training_log/2/delay_time_30_log.txt
+python3 dqn_training.py --delay_time 40 >> ./training_log/2/delay_time_40_log.txt
 python3 dqn_training.py --delay_time 50 >> ./training_log/2/delay_time_50_log.txt
+python3 dqn_training.py --delay_time 60 >> ./training_log/2/delay_time_60_log.txt
+python3 dqn_training.py --delay_time 70 >> ./training_log/2/delay_time_70_log.txt
+python3 dqn_training.py --delay_time 80 >> ./training_log/2/delay_time_80_log.txt
+python3 dqn_training.py --delay_time 90 >> ./training_log/2/delay_time_90_log.txt
 python3 dqn_training.py --delay_time 100 >> ./training_log/2/delay_time_100_log.txt
-python3 dqn_training.py --delay_time 150 >> ./training_log/2/delay_time_150_log.txt
-python3 dqn_training.py --delay_time 200 >> ./training_log/2/delay_time_200_log.txt
-python3 dqn_training.py --delay_time 300 >> ./training_log/2/delay_time_300_log.txt
-python3 dqn_training.py --delay_time 400 >> ./training_log/2/delay_time_400_log.txt
-python3 dqn_training.py --delay_time 500 >> ./training_log/2/delay_time_500_log.txt
