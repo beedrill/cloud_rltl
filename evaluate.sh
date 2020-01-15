a71394d3a3863dafd9243e337e57eaf09735d8c9
XINZE ZHOU <zhouxinze@192.168.0.2>
2020-01-15 13:20:20 +0800

    Add train.sh and evaluate.sh, describe its usage in README.md

--- evaluate.sh
+++ evaluate.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-evaluate_index = 1
-python3 dqn_evaluation2.py --folder_name 'DRL_50' --delay_time 0 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_51' --delay_time 1 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_52' --delay_time 2 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_53' --delay_time 3 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_54' --delay_time 5 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_55' --delay_time 10 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_56' --delay_time 15 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_57' --delay_time 20 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_58' --delay_time 25 >> ./evaluation_result/evaluation_${evaluate_index}.txt
-python3 dqn_evaluation2.py --folder_name 'DRL_59' --delay_time 30 >> ./evaluation_result/evaluation_${evaluate_index}.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_50' --delay_time 0 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_51' --delay_time 1 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_52' --delay_time 2 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_53' --delay_time 3 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_54' --delay_time 5 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_55' --delay_time 10 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_56' --delay_time 15 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_57' --delay_time 20 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_58' --delay_time 25 >> ./evaluation_result/evaluation_5.txt
+python3 dqn_evaluation2.py --folder_name 'DRL_59' --delay_time 30 >> ./evaluation_result/evaluation_5.txt
--- train.sh
+++ train.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-train_index = 1
-python3 dqn_training.py --delay_time 0 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 1 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 2 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 3 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 5 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 10 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 15 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 20 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 25 >> ./training_log/training_${train_index}_log.txt
-python3 dqn_training.py --delay_time 30 >> ./training_log/training_${train_index}_log.txt
+python3 dqn_training.py --delay_time 0 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 1 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 2 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 3 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 5 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 10 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 15 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 20 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 25 >> ./training_log/training_1_log.txt
+python3 dqn_training.py --delay_time 30 >> ./training_log/training_1_log.txt
