c2ce7c1bdec964291e36ede815107eabf0437802
XINZE ZHOU <zhouxinze@192.168.0.9>
2020-01-31 14:58:04 +0800

    train.sh

--- train.sh
+++ train.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-python3 dqn_training.py --delay_time 0 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 1 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 2 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 3 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 5 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 10 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 15 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 20 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 25 --env_option 1 >> ./training_log/training_2_log.txt
-python3 dqn_training.py --delay_time 30 --env_option 1 >> ./training_log/training_2_log.txt
+python3 dqn_training.py --delay_time 0 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 1 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 2 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 3 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 4 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 5 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 6 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 7 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 8 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 9 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 10 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 11 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 12 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 13 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 14 --env_option 0 >> ./training_log/training_3_log.txt
+python3 dqn_training.py --delay_time 15 --env_option 0 >> ./training_log/training_3_log.txt
